15ba5fa40914d4f8b30284e838b4392a359ddfa2
armandoxd03 <johnroyducut04@gmail.com>
2025-02-20 12:13:18 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,55 @@
 import { useState } from 'react';
-import { animeList } from './data.tsx';
+import { skillList } from './data.tsx';
 import './App.css';
-import { 
-  Card, CardMedia, CardContent, CardActions, Collapse, 
-  IconButton, Typography 
-} from '@mui/material';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
-export default function AnimeCard() {
+export default function Gallery() {
   const [index, setIndex] = useState(0);
-  const [expanded, setExpanded] = useState(false);
-  const hasNext = index < animeList.length - 1;
+  const [showMore, setShowMore] = useState(false);
+  const hasNext = index < skillList.length - 1;
   const hasBack = index > 0;
 
   function handleNextClick() {
-    setIndex(hasNext ? index + 1 : 0);
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
   }
 
   function handleBackClick() {
-    setIndex(hasBack ? index - 1 : animeList.length - 1);
+    if (hasBack) {
+      setIndex(index - 1);
+    } else {
+      setIndex(skillList.length - 1);
+    }
   }
 
-  function handleExpandClick() {
-    setExpanded(!expanded);
+  function handleMoreClick() {
+    setShowMore(!showMore);
   }
 
-  let anime = animeList[index];
-
+  let skill = skillList[index];
   return (
-    <div className="anime-box">
-      <div className="anime-header">
-      <IconButton aria-label="back" onClick={handleBackClick} sx={{ color: 'white' }}>
-        <ArrowBackIcon />
-      </IconButton>
+    <>
+    <h1>10 Things that Require Zero Talent</h1>
+    <h3>John Roy C. Ducut</h3>
 
-        <Card sx={{ maxWidth: 345 }} className="anime-card">
-          <CardContent>
-            <Typography variant="h5" component="h2" className="anime-title">
-              John Roy Ducut - C-PEITEL3
-            </Typography>
-            <Typography variant="h6" component="h3">
-              My Anime List
-            </Typography>
-            <Typography variant="subtitle1" component="h3">
-              {index + 1} of {animeList.length}
-            </Typography>
-          </CardContent>
-
-          <CardMedia
-            component="img"
-            image={anime.url}
-            alt={anime.alt}
-            className="anime-image"
-          />
-
-          <CardContent>
-            <Typography variant="h6" className="anime-title">
-              {anime.name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              by {anime.artist}
-            </Typography>
-          </CardContent>
-
-          <CardActions className="card-actions" disableSpacing>
-            <IconButton
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-              sx={{
-                transform: expanded ? 'rotate(180deg)' : 'rotate(0deg)',
-                transition: 'transform 0.3s ease',
-              }}
-            >
-              <ExpandMoreIcon />
-            </IconButton>
-          </CardActions>
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <CardContent>
-              <Typography>{anime.description}</Typography>
-            </CardContent>
-          </Collapse>
-        </Card>
-
-        <IconButton aria-label="next" onClick={handleNextClick} sx={{ color: 'white' }}>
-          <ArrowForwardIcon />
-        </IconButton>
+      {showMore && <p>{skill.detail}</p>}
+      <img
+        src={skill.url}
+        alt={skill.alt}
+      />
+            <div>
+            <h2>
+        <i>{skill.name} </i>
+      </h2>
+      <p><i>{skill.detail}</i></p>
+      <button onClick={handleBackClick}>
+        Back
+      </button>
+      <button onClick={handleNextClick}>
+        Next
+      </button>
       </div>
-    </div>
+    </>
   );
 }
